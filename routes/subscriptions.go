6a6d79fbfa0ccc3b707a750135fba61b4ac58c7a
David Wurtz <davidjwurtz@gmail.com>
2022-08-19 13:53:49 -0700

    routes for saasapi

---
+++ routes/subscriptions.go
@@ -0,0 +1,47 @@
+package routes
+
+import (
+	"peachone/saasapi"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+// --------------------------------------------------------------------------------
+// Resolve handler
+// --------------------------------------------------------------------------------
+type ResolveRequest struct {
+	Token string `json:"token"`
+}
+
+type ResolveResponse struct {
+	Success              bool                         `json:"success"`
+	ResolvedSubscription saasapi.ResolvedSubscription `json:"resolvedSubscription"`
+}
+
+func Resolve(c *fiber.Ctx) error {
+	// get request body
+	req := new(ResolveRequest)
+	if err := c.BodyParser(req); err != nil {
+		return err
+	}
+
+	// validate request body
+	if req.Token == "" {
+		return fiber.NewError(fiber.StatusBadRequest, "Invalid token.")
+	}
+
+	// create fulfillment api client
+	client := saasapi.NewDefaultFulfillmentOperationsClient()
+
+	resp, err := client.Resolve(c.Context(), req.Token, nil)
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, "Could not resolve token.")
+	}
+
+	// return response
+	response := &ResolveResponse{
+		Success:              true,
+		ResolvedSubscription: resp.ResolvedSubscription,
+	}
+	return c.JSON(response)
+}
